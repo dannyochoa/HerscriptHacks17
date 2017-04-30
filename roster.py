e2e871fd2c5fe12402ee2810688876edb2d8aa2a
sampsonliao <sliao@csumb.edu>
2017-04-30 03:34:52 -0700

    Ability to add workers into file

--- roster.py
+++ roster.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 
 def get_roster(filepath):
@@ -11,9 +12,21 @@ def get_roster(filepath):
     roster_df.reset_index(inplace=True)
     del roster_df['index']
     roster_dict = roster_df.set_index('id').T.to_dict('list')
-    print(roster_dict)
     return roster_dict
+
+
+def write_to(id, name, crew, filepath):
+    """Write directly to csv file without overwriting with new file.
+    Takes in arguments of type str: 
+                write_to("<id_str>", "<name_str>", <crew_int>, "<filepath_str>")
+    example:    write_to("21452", "Daniel Ochoa", 1, "Data/roster.csv")
+    """
+    with open(filepath, 'a', newline='') as csvfile:
+        # csvfile.write([id,name,crew])
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow([id, name, crew])
 
 
 if __name__ == "__main__":
     roster_dict2 = get_roster("Data/roster.csv")
+    # write_to("21452", "Daniel Ochoa", 1, "Data/roster.csv")
