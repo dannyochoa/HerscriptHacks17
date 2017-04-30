a36f27f3fabb9f4dd5c0c3a9181c5f6cdab723ef
sampsonliao <sliao@csumb.edu>
2017-04-29 23:31:38 -0700

    Created roster for workers

--- data_generator.py
+++ data_generator.py
@@ -9,7 +9,9 @@ from bokeh.models import HoverTool
 import math
 
 
-def gen_liter(data):
+def gen_liter(datetime):
+    data = int(convert_tonum(datetime))
+    # data = dat/etime
     # df.loc[(df['date'] == 1), 'liters'] = np.random.randint(0,100)
     num = 0
     if data == 1:
@@ -39,7 +41,8 @@ def gen_liter(data):
     return num
 
 
-def gen_energy(data):
+def gen_energy(datetime):
+    data = int(datetime)
     # df.loc[(df['date'] == 1), 'liters'] = np.random.randint(0,100)
     num = 0
     if data == 1:
@@ -69,52 +72,49 @@ def gen_energy(data):
     return num
 
 
+def convert_tonum(data):
+    my_date = datetime.strptime(data, "%Y-%m-%d")
+    return my_date.strftime("%m")
+
 def convert_todatetime(data):
-    my_date = datetime.strptime(str(data), "%m")
-    return my_date.strftime("%m")  # 23 Dec, 2011
+    my_date = datetime.strptime(data, "%m")
+    return my_date.strftime("2017-%m-00")
 
 
-df = pd.read_csv('Data/randmonth.csv', header=0, names=['date'])
+df = pd.read_csv('Data/datetime.csv', header=0, names=['date'])
 
-date = pd.date_range(start='1-01-17', end='12-01-17', freq='MS')
+# date = pd.date_range(start='1-01-17', end='12-01-17', freq='MS')
+df['month'] = df['date'].apply(convert_tonum)
+
 df['liter'] = df['date'].apply(gen_liter)
-df['energy'] = df['date'].apply(gen_energy)
-df['month'] = df['date'].apply(convert_todatetime)
-df_liter = pd.DataFrame({'liter': df['liter'], 'date': df['date'], 'month': df['month']}, columns=['liter', 'date','month'])
-df_energy = pd.DataFrame({'energy': df['energy'], 'date': df['date'], 'month': df['month']}, columns=['energy', 'date', 'month'])
-df_liter.sort_values('date', inplace=True)
-df_energy.sort_values('date', inplace=True)
-print(df_liter)
 
-df_liter_grouped = df_liter.groupby('date').mean()
-print(df_liter_grouped)
-df_energy_grouped = df_energy.groupby('date').mean()
-# df_liter_grouped['datetime'] = date
-df_liter_grouped['datetime'] = df_liter_grouped.index
-df_liter_grouped['datetime'] = df_liter_grouped['datetime'].apply(convert_todatetime)
+df_liter = pd.DataFrame({'liter': df['liter'], 'date': df['date'], 'month': df['month']},
+                        columns=['liter', 'date', 'month'])
 
-# df_liter_grouped.set_index(['datetime'])
-# df_liter_grouped['Month'] = df_liter_grouped['datetime'].apply(lambda x: x.strftime('%b'))
-print(df_liter_grouped)
+df_liter.sort_values('month', inplace=True)
 
+df_liter_grouped = df_liter.groupby('month').mean()
+df_liter_grouped['datetime'] = df_liter_grouped.index.values
 
-df.plot(kind='scatter', x='date', y='liter')
-df.plot(kind='scatter', x='date', y='energy')
-
-# df_liter.plot()
-
+df_liter_grouped['datetime'] = df_liter_grouped['datetime']. apply(convert_todatetime)
+# df_liter_grouped['month2'] = df_liter_grouped.index
 
 output_file("line.html")
 
 p = figure(plot_width=1000, plot_height=400, title="Water usage L/kg/y")
 
 # add a circle renderer with a size, color, and alpha
+print(df_liter_grouped)
 
-
-p.circle(df['datetime'], df['liter'], size=20, color="navy", alpha=0.5)
+# p.circle(df['month'], df['liter'], size=20, color="navy", alpha=0.5)
 p.line(df_liter_grouped['datetime'], df_liter_grouped['liter'])
 
-p.xaxis[0].formatter = DatetimeTickFormatter(months="%b")
+p.xaxis[0].formatter=DatetimeTickFormatter(
+        hours=["%B"],
+        days=["%B"],
+        months=["%B"],
+        years=["%B"],
+    )
 
 p.xaxis.axis_label = "Month"
 p.yaxis.axis_label = "L/kg"
